8edf61e3df76a6d16e7559137faf2a89274ca7e0
Mohamed Sayed <ms10596@gmail.com>
2018-11-20 15:31:16 +0200

    feat: triangle graph

---
+++ triangle.py
@@ -0,0 +1,23 @@
+class Triangle:
+    def __init__(self, x1, x2, x3):
+        self.x1 = x1
+        self.x2 = x2
+        self.x3 = x3
+
+        self.m1 = 1 / (x2 - x1)
+        self.m2 = -1 / (x3 - x2)
+        print(self.m1, self.m2)
+
+    def predict(self, x):
+        if self.x1 <= x <= self.x2:
+            return self.m1 * x
+        elif self.x2 < x <= self.x3:
+            return self.m2 * (x - self.x2) + 1
+        else:
+            return 0
+
+
+if __name__ == '__main__':
+    t = Triangle(0, 25, 1000)
+    for i in range(1000):
+        print(i, t.predict(i))
