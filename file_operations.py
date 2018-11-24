407ec515b93aede7df1877c767a696ac98af95c4
Mohamed Sayed <ms10596@gmail.com>
2018-11-24 11:11:22 +0200

    refactor: reading functions moved

---
+++ file_operations.py
@@ -0,0 +1,56 @@
+from variable import Variable
+from logic.premise import Premise
+from logic.rule import Rule
+
+
+def read_variables(f):
+    variables = []
+    vars_n = int(f.readline())
+    for i in range(vars_n):
+        line = f.readline().split()
+        name = line[0]
+        value = float(line[1])
+        new_var = Variable(name, value)
+        fuzzy_sets_n = int(f.readline())
+        for j in range(fuzzy_sets_n):
+            line = f.readline().split()
+            name = line[0]
+            # kind = line[1]
+
+            values = tuple(map(float, f.readline().split()))
+            new_var.add_fuzzy_set(name, values)
+        variables.append(new_var)
+    name = f.readline().split()[0]
+    new_var = Variable(name)
+    fuzzy_sets_n = int(f.readline())
+    for i in range(fuzzy_sets_n):
+        line = f.readline().split()
+        name = line[0]
+        # kind = line[1]
+
+        values = tuple(map(float, f.readline().split()))
+        new_var.add_fuzzy_set(name, values)
+
+    variables.append(new_var)
+    return variables
+
+
+def read_rules(f):
+    rules = []
+    rules_n = int(f.readline())
+
+    for i in range(rules_n):
+        new_rule = Rule()
+        line = f.readline().split()
+        premises_n = int(line[0])
+        for j in range(premises_n):
+            first_premise = Premise(line[1 + j * 4], line[3 + j * 4])
+            new_rule.add_premise(first_premise)
+            if j < premises_n - 1:
+                gate = line[4 + j]
+                new_rule.add_premise(gate)
+        last_premise = Premise(line[len(line) - 3], line[len(line) - 1])
+        new_rule.add_premise(last_premise)
+
+        rules.append(new_rule)
+    return rules
